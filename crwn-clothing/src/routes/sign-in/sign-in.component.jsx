86b9a332d05df34028343c1047e050bb9fd31df1
Rajat Tanwar <rajtanwa@WKMZTFFE7248.global.publicisgroupe.net>
2023-06-14 23:45:29 +0530

    Created Sign Up Component, Form Input Component, Button component

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,44 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
 
-    const googleUser = async () => {
-        const {user} = await signInWithGooglePopup();
+    // useEffect( async () => {
+    //     const response = await getRedirectResult(auth);
+    //     if (response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // }, []);
+
+    const logGoogleRedirectUser = async() => {
+        const response = await getRedirectResult(auth);
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }
+
+    useEffect( () => {
+        logGoogleRedirectUser();
+    }, []);
+
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
+
+    // const logGoogleRedirectUser = async () => {
+    //     const {user} = await signInWithGoogleRedirect();
+    //     console.log(user);
+    // }
+
     return (
         <div>
             <h1>I'm the sign in COMPONENT</h1>
-            <button onClick={googleUser}>Sign In With Google Popup</button>
+            <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
+            <SignUpForm />
         </div>
     )
 }
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
+import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider, createUserWithEmailAndPassword} from 'firebase/auth';
 import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,17 +18,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider= new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider= new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account"
 })
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider);
 
 export const db= getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth) => {
+export const createUserDocumentFromAuth = async(userAuth, additionalInformation = {}) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
@@ -37,11 +38,16 @@ export const createUserDocumentFromAuth = async(userAuth) => {
         const createdAt = new Date();
 
         try{
-            await setDoc(userDocRef,{displayName, createdAt,email});
+            await setDoc(userDocRef,{displayName, createdAt,email,...additionalInformation});
         }catch(error){
             console.log(`error creating the user`, error.message);
         }
     }
 
     return userDocRef;
+}
+
+export const createAutUserWithEmailAndPassword = async(email, password) => {
+    if(!email || !password) return;
+    return await createUserWithEmailAndPassword(auth,email,password);
 }
