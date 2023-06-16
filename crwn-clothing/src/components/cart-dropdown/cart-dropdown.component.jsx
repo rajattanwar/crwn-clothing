fa0d159d44b673c3361827572d59d9ce3053da12
Rajat Tanwar <rajtanwa@WKMZTFFE7248.global.publicisgroupe.net>
2023-06-16 11:46:25 +0530

    Add To Cart Feature added

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
+import { useContext } from 'react';
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
-    
-    return(
+
+    const { addItemToCart, cartItems } = useContext(CartContext);
+
+    return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>Go To Checkout</Button>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
+
             </div>
+            <Button>Go To Checkout</Button>
         </div>
     )
 }
